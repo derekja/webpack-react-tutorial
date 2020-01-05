866900372abea41de0330ca0c3bbe0ab7b77b1b0
Derek jacoby <derekja@gmail.com>
2020-01-05 14:47:45 -0800

    update to a working version. No create-react-app, but external ink.js is built from 1.8.0

---
+++ src/play.js
@@ -0,0 +1,16 @@
+﻿// play.js
+
+import React, { Component } from 'react';
+import './play.css';
+
+class Play extends Component {
+  render() {
+    return (
+        <div>
+          <h2>play</h2>
+        </div>
+    );
+  }
+}
+
+export default Play;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
-﻿const HtmlWebPackPlugin = require("html-webpack-plugin");
+﻿const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const webpackDevServer = require('webpack-dev-server');
+
+const port = process.env.PORT || 3000;
+
 module.exports = {
+  entry: './src/index.js',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loaders: ["babel-loader"]
       },
       {
         test: /\.html$/,
@@ -16,13 +21,52 @@ module.exports = {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[hash].[ext]",
+          },
+        },
       }
     ]
   },
+  devtool: 'inline-source-map',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
     })
-  ]
+  ],
+  devServer: {
+    publicPath: "/",
+    contentBase: "./public",
+    hot: true,
+    host: 'localhost',
+    port: port,
+    historyApiFallback: {
+      index: '/'
+    },
+    open: true
+  }
 };
